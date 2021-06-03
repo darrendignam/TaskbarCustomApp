e3cc9cb21a1b35e8d878bff3b544630f88a94cde
Darren Dignam <darren.dignam@gmail.com>
2021-06-03 09:48:21 +0200

    Added a regirstry key to enable autostart

--- TBCA/About.Designer.cs
+++ TBCA/About.Designer.cs
@@ -33,6 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.cbAutostart = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +41,7 @@
             // 
             this.pictureBox1.Image = global::TBCA.Properties.Resources.tickertape_black_transparent;
             this.pictureBox1.Location = new System.Drawing.Point(7, 7);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(423, 48);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -60,9 +61,10 @@
             // 
             // textBox1
             // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.textBox1.Enabled = false;
             this.textBox1.Location = new System.Drawing.Point(7, 99);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(425, 190);
@@ -79,17 +81,29 @@
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "https://github.com/darrendignam/TaskbarCustomApp";
             // 
+            // cbAutostart
+            // 
+            this.cbAutostart.AutoSize = true;
+            this.cbAutostart.Location = new System.Drawing.Point(7, 317);
+            this.cbAutostart.Name = "cbAutostart";
+            this.cbAutostart.Size = new System.Drawing.Size(174, 17);
+            this.cbAutostart.TabIndex = 4;
+            this.cbAutostart.Text = "Start program when user logs in";
+            this.cbAutostart.UseVisualStyleBackColor = true;
+            this.cbAutostart.CheckedChanged += new System.EventHandler(this.cbAutostart_CheckedChanged);
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 314);
+            this.ClientSize = new System.Drawing.Size(436, 346);
+            this.Controls.Add(this.cbAutostart);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "About";
             this.Text = "TBCA Task Launcher";
             this.Load += new System.EventHandler(this.About_Load);
@@ -105,5 +119,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox cbAutostart;
     }
 }
--- TBCA/About.cs
+++ TBCA/About.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,12 +19,47 @@ namespace TBCA
 
         private void About_Load(object sender, EventArgs e)
         {
-
+            cbAutostart.Checked = isAutoStartEnabled();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public static bool isAutoStartEnabled()
+        {
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (!regKey.GetValueNames().Contains("TBCA-Task-Launcher"))
+            {
+                return false;
+            }
+            else
+            {
+                if (regKey.GetValue("TBCA-Task-Launcher").ToString() == "False" )
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        private void cbAutostart_CheckedChanged(object sender, EventArgs e)
+        {
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (cbAutostart.Checked)
+            {
+                regKey.SetValue("TBCA-Task-Launcher", Application.ExecutablePath);
+            }
+            else
+            {
+                regKey.SetValue("TBCA-Task-Launcher", false);
+            }
         }
     }
 }
